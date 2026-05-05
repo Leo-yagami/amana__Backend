dfd33a984a065f9f4c509ce8e580f58365e0d393
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-05-05 20:23:40 +0300

    fix

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,12 +8,19 @@ const generateToken = (res, userId,userName, userEmail) => {
   );
 
   // HttpOnly cookie: JS cannot read this, preventing XSS
-  res.cookie('token', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // HTTPS only in prod
-    sameSite: 'strict', // CSRF protection
-    maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-  });
+  // res.cookie('token', token, {
+  //   httpOnly: true,
+  //   secure: process.env.NODE_ENV === 'production', // HTTPS only in prod
+  //   sameSite: 'strict', // CSRF protection
+  //   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+  // });
+
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path: "/",
+});
   
   return token;
 };
