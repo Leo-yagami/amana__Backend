7676aefe2feaa44044a7e06ba92c82ede6f9ee69
Haitham Abdulhamid <118968574+Leo-yagami@users.noreply.github.com>
2026-05-06 18:12:17 +0300

    replaced old url with new

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'https://amana-bckend-api.vercel.app/api/auth/google/callback',
+      callbackURL: 'https://api-amana-fullstack.onrender.com/api/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
